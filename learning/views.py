d9b185b0e81fe67777b79484a95a06874476ae8d
zhangsan <1299940308@qq.com>
2025-01-19 00:04:18 +0800

    优化代码：完善删除逻辑，在单词列表删除单词时会把对应的单词音频数据删除

--- learning/views.py
+++ learning/views.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import random
+import time
+from datetime import timedelta
 from pathlib import Path
 
 from django.conf import settings
@@ -7,9 +10,13 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.core.files import File
 from django.core.files.storage import default_storage
+from django.db import transaction
+from django.db.models import Subquery, OuterRef
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Word, AudioFile
+from django.utils import timezone
+
+from .models import Word, AudioFile, UserWordProgress
 import requests
 from lxml import html
 
@@ -17,6 +24,8 @@ from lxml import html
 
 # 设置日志配置
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# 复习间隔：1天、2天、4天、7天、15天
+REVIEW_INTERVALS = [1, 2, 4, 7, 15]
 
 
 def home(request):
@@ -54,36 +63,64 @@ def word_list(request):
     return render(request, 'learning/word_list.html', {'words': words})
 
 
-
 def word_card(request):
-    # 这里实现单词卡片的功能
-    random_word = Word.objects.order_by('?').first()
+    start_time = time.time()
 
-    # 初始化变量
-    phonetic = ""
-    uk_pronunciation_link = ""
-    us_pronunciation_link = ""
+    try:
+        # 获取所有在 Word 表中但不在 AudioFile 表中的数据的 ID 列表
+        word_ids = list(Word.objects.exclude(
+            word__in=AudioFile.objects.values('word_text')
+        ).values_list('id', flat=True))
 
-    # 如果单词没有中文意思，调用翻译 API 获取并保存
-    if not random_word.definition:
-        phonetic, chinese_meaning, uk_pronunciation_link, us_pronunciation_link = get_youdao_data(random_word)
-        if chinese_meaning:
-            random_word.definition = chinese_meaning
-            random_word.phonetic = phonetic
-            random_word.save()  # 保存到数据库
+        if not word_ids:
+            logging.warning("No words found in Word table that are not in AudioFile table.")
+            return render(request, 'learning/word_card.html', {'word': None})
 
-    if not random_word.phonetic_uk and uk_pronunciation_link:
-        download_and_save_audio(uk_pronunciation_link, random_word, "uk")
+        # 随机选择一个 ID
+        random_word_id = random.choice(word_ids)
+        random_word = Word.objects.get(id=random_word_id)
+        logging.info(f"Selected word is: {random_word}")
+        # 初始化变量
+        phonetic = ""
+        uk_pronunciation_link = ""
+        us_pronunciation_link = ""
 
-    if not random_word.phonetic_us and us_pronunciation_link:
-        download_and_save_audio(us_pronunciation_link, random_word, "us")
+        # 如果单词没有中文意思，调用翻译 API 获取并保存
+        if not random_word.definition or not random_word.phonetic_uk or not random_word.phonetic_us:
+            try:
+                phonetic, chinese_meaning, uk_pronunciation_link, us_pronunciation_link = get_youdao_data(random_word)
+                if chinese_meaning:
+                    with transaction.atomic():
+                        random_word.definition = chinese_meaning
+                        random_word.phonetic = phonetic
+                        random_word.save()
+            except Exception as e:
+                logging.error(f"Failed to get Youdao data: {e}")
 
-    logging.info(f"Word: {random_word}, Phonetic: {phonetic}")
-    logging.info(f"UK Pronunciation Link: {uk_pronunciation_link}")
-    logging.info(f"US Pronunciation Link: {us_pronunciation_link}")
+        if not random_word.phonetic_uk and uk_pronunciation_link:
+            try:
+                download_and_save_audio(uk_pronunciation_link, random_word, "uk")
+            except Exception as e:
+                logging.error(f"Failed to download UK pronunciation audio: {e}")
+
+        if not random_word.phonetic_us and us_pronunciation_link:
+            try:
+                download_and_save_audio(us_pronunciation_link, random_word, "us")
+            except Exception as e:
+                logging.error(f"Failed to download US pronunciation audio: {e}")
+
+        logging.info(f"Word: {random_word.word}, Phonetic: {phonetic}")
+        logging.info(f"UK Pronunciation Link: {uk_pronunciation_link}")
+        logging.info(f"US Pronunciation Link: {us_pronunciation_link}")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return render(request, 'learning/word_card.html', {'word': None})
+
+    end_time = time.time()
+    logging.info(f"查询耗时：{end_time - start_time}秒")
 
     return render(request, 'learning/word_card.html', {'word': random_word})
-
 
 
 def download_and_save_audio(url, word, language):
@@ -137,10 +174,12 @@ def download_and_save_audio(url, word, language):
 # 提供单词音频地址
 def get_audio_url(request, word):
     # audio_files = get_object_or_404(AudioFile, word_text=word)
-    audio_file = AudioFile.objects.filter(word_text=word, language='us')
-
+    audio_file = AudioFile.objects.filter(word_text=word, language='us').first()
+    if audio_file is None:
+        logging.error(f"未找到单词 {word} 的美式发音音频文件")
+        return JsonResponse({'error': '未找到音频文件'}, status=404)
     # 获取数据库中的路径
-    db_path = audio_file.first().file_path
+    db_path = audio_file.file_path
     # 构建完整的音频文件 URL
     # audio_url = settings.MEDIA_ROOT / db_path
     audio_url = str(db_path)
@@ -211,6 +250,150 @@ def add_words(request):
 
 
 def delete_word(request, word_id):
-    word = get_object_or_404(Word, id=word_id)
-    word.delete()
+    """
+    删除指定的单词对象及其关联的音频文件。
+
+    此函数使用事务确保数据一致性。在删除单词对象之前，它首先删除与该单词关联的所有音频文件。
+    如果在删除过程中遇到任何错误，将记录错误信息并回滚事务。
+
+    参数:
+    - request: HTTP 请求对象，包含请求信息。
+    - word_id: 要删除的单词的主键标识符。
+
+    返回:
+    - 成功删除后重定向到单词列表页面。
+    - 如果删除过程中发生错误，重定向到错误页面。
+    """
+    try:
+        with transaction.atomic():
+            # 使用 try-except 块来处理获取单词对象时可能抛出的异常
+            word = get_object_or_404(Word, id=word_id)
+
+            # 获取关联的 AudioFile 记录
+            audio_files = AudioFile.objects.filter(word_text=word.word)
+            for audio_file in audio_files:
+                if audio_file.file_path:
+                    # 构建文件的完整路径并进行规范化，以确保路径安全
+                    file_path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, audio_file.file_path))
+                    # 检查文件是否存在且路径正确，然后尝试删除文件
+                    if os.path.exists(file_path) and str(file_path).startswith(str(settings.MEDIA_ROOT)):
+                        try:
+                            os.remove(file_path)
+                            logging.info(f"已删除音频文件: {file_path}")
+                        except OSError as e:
+                            logging.error(f"删除音频文件失败: {file_path}, 错误信息: {e}")
+
+                try:
+                    # 尝试从数据库中删除 AudioFile 记录
+                    audio_file.delete()
+                except Exception as e:
+                    logging.error(f"删除 AudioFile 数据库记录失败: {audio_file.id}, 错误信息: {e}")
+
+            try:
+                # 尝试从数据库中删除单词对象
+                word.delete()
+            except Exception as e:
+                logging.error(f"删除单词对象失败: {word.id}, 错误信息: {e}")
+                raise  # 重新抛出异常以触发事务回滚
+
+    except Exception as e:
+        logging.error(f"删除单词过程中发生错误: {e}")
+        # 根据需求决定是否重定向到错误页面或返回错误响应
+        return redirect('word_list')
+
     return redirect('word_list')  # 重定向到单词列表页面
+
+# def start_learning_new_word(user, word_id):
+#     """
+#     用户开始学习新单词，初始化用户对单词的学习进度。
+#     :param user: 当前用户
+#     :param word_id: 单词ID
+#     """
+#     word = Word.objects.get(id=word_id)  # 获取指定ID的单词
+#
+#     # 创建学习进度记录
+#     progress_instance = UserWordProgress.objects.create(
+#         user=user,
+#         word=word,
+#         review_count=0,  # 初次学习时复习次数为0
+#         last_review_time=timezone.now(),
+#         next_review_time=timezone.now() + timedelta(days=REVIEW_INTERVALS[0]),  # 初次复习时间设为1天后
+#         review_interval=REVIEW_INTERVALS[0],  # 初次复习间隔为1天
+#         status=0  # 设置为新单词状态
+#     )
+#
+#     return progress_instance
+#
+#
+# def update_review_progress(progress_instance, rating):
+#     """
+#     根据用户评分（1-5）更新复习进度。
+#     :param progress_instance: 当前单词的学习进度实例
+#     :param rating: 用户评分，1-5之间
+#     """
+#     # 用户评分较低时，复习间隔缩短
+#     if rating in [1, 2]:
+#         # 如果评分较低，复习间隔缩短，最多退回1天
+#         progress_instance.review_interval = max(1, progress_instance.review_interval - 1)
+#     elif rating >= 3:
+#         # 如果评分较高，增加复习间隔
+#         current_index = REVIEW_INTERVALS.index(progress_instance.review_interval)
+#         if current_index < len(REVIEW_INTERVALS) - 1:
+#             progress_instance.review_interval = REVIEW_INTERVALS[current_index + 1]  # 增加间隔
+#
+#     # 更新复习日期
+#     progress_instance.next_review_time = timezone.now() + timedelta(days=progress_instance.review_interval)
+#     progress_instance.review_count += 1  # 增加复习次数
+#     progress_instance.last_review_time = timezone.now()  # 更新最近复习时间
+#     progress_instance.save()
+#
+#
+# def get_words_to_review(user):
+#     """
+#     获取当前用户需要复习的单词。
+#     :param user: 当前用户
+#     :return: 需要复习的单词列表
+#     """
+#     return UserWordProgress.objects.filter(user=user, next_review_time__lte=timezone.now())
+#
+#
+# def review_word(request):
+#     """
+#     用户复习单词的逻辑，显示需要复习的单词并更新进度。
+#     :param request: 请求对象
+#     :return: 渲染复习页面
+#     """
+#     user = request.user
+#     words_to_review = get_words_to_review(user)  # 获取需要复习的单词
+#
+#     if words_to_review.exists():
+#         word_to_review = words_to_review[0].word  # 获取待复习的单词
+#
+#         if request.method == 'POST':
+#             # 用户提交评分
+#             rating = int(request.POST['rating'])  # 假设评分是1-5
+#             progress_instance = UserWordProgress.objects.get(user=user, word=word_to_review)
+#             update_review_progress(progress_instance, rating)  # 更新复习进度
+#             return redirect('review_word')  # 重定向到复习页面
+#
+#         return render(request, 'review_word.html', {'word': word_to_review})
+#     else:
+#         return render(request, 'no_words_to_review.html')  # 如果没有单词需要复习，显示无单词页面
+#
+#
+# def learn_new_word(request):
+#     """
+#     引导用户学习新单词，随机选择未学习的单词。
+#     :param request: 请求对象
+#     :return: 渲染学习新单词页面
+#     """
+#     user = request.user  # 当前用户
+#     unlearned_words = Word.objects.exclude(id__in=UserWordProgress.objects.filter(user=user).values('word_id'))
+#
+#     if unlearned_words.exists():
+#         # 从未学习的单词中随机选择一个
+#         word_to_learn = random.choice(unlearned_words)  # 随机选择一个未学习的单词
+#         start_learning_new_word(user, word_to_learn.id)  # 开始学习该单词
+#         return redirect('learn_word_page', word_id=word_to_learn.id)  # 跳转到学习该单词页面
+#     else:
+#         return render(request, 'all_words_learned.html')  # 如果所有单词都已学习，提示用户
