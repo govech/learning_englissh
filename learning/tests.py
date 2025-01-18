9194034c493f000b1cc8cf1fca9568f587c10c5b
zhangsan <1299940308@qq.com>
2025-01-18 19:05:14 +0800

    新增：1、初始化数据库（为单词下载音标、音频、翻译） 2 使用Django单元测试运行初始化脚本：python manage.py test learning.tests.WordCardTests

--- learning/tests.py
+++ learning/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from learning.my_utils.init_db_and_audio import word_card
 
-# Create your tests here.
+class WordCardTests(TestCase):
+    def test_word_card(self):
+        result = word_card()
+        self.assertIsNotNone(result)  # 根据实际需求添加断言
+        print(f'Test result: {result}')
